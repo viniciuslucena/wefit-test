214584ebf70323bba746534fb795537f5ea28e2e
Vinicius <jviniiciuslucena@gmail.com>
2022-10-11 14:22:02 -0300

    feat: create header structure

---
+++ wefit-test-react/wemovies/src/components/Header/index.jsx
@@ -0,0 +1,10 @@
+import * as S from './styles'
+
+export const Header = () => {
+  return (
+    <S.Header>
+      <h1>WeMovies</h1>
+      <p>Teste</p>
+    </S.Header>
+  )
+}
---
+++ wefit-test-react/wemovies/src/components/Header/styles.js
@@ -0,0 +1,13 @@
+import styled from 'styled-components'
+
+export const Header = styled.div`
+  width: 100%;
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  padding: 2.4rem 0;
+
+  h1 {
+    font-size: 2rem;
+  }
+`
