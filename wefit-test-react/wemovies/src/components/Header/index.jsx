01ba12afb2405430fc63a727028f359d0ca8fd71
Vinicius <jviniiciuslucena@gmail.com>
2022-10-11 16:42:46 -0300

    refactor: change the number of items in cart based in array length

--- wefit-test-react/wemovies/src/components/Header/index.jsx
+++ wefit-test-react/wemovies/src/components/Header/index.jsx
@@ -1,18 +1,26 @@
-import { IoMdBasket } from 'react-icons/io'
+import { useContext } from "react";
+import { IoMdBasket } from "react-icons/io";
 
-import * as S from './styles'
+import { CartContext } from "../../context/CartContext";
+import * as S from "./styles";
 
 export const Header = () => {
+  const { productsInCart } = useContext(CartContext);
+
   return (
     <S.Header>
       <h1>WeMovies</h1>
       <S.BasketSession>
         <div>
-          <p className='myCart'>Meu Carrinho</p>
-          <p className='totalItens'>0 itens</p>
+          <p className="myCart">Meu Carrinho</p>
+          {productsInCart.length === 1 ? (
+            <p className="totalItens">{productsInCart.length} item</p>
+          ) : (
+            <p className="totalItens">{productsInCart.length} itens</p>
+          )}
         </div>
-        <IoMdBasket size='32px' />
+        <IoMdBasket size="32px" />
       </S.BasketSession>
     </S.Header>
-  )
-}
+  );
+};
