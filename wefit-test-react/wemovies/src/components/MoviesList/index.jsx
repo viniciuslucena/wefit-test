8217355afd0cbc5ffb122f2c0394bfcb15eeee1a
Vinicius <jviniiciuslucena@gmail.com>
2022-10-11 15:42:41 -0300

    feat: Movies list component

--- wefit-test-react/wemovies/src/components/MoviesList/index.jsx
+++ wefit-test-react/wemovies/src/components/MoviesList/index.jsx
@@ -1,5 +1,42 @@
+import { useEffect, useState } from "react"
+import { ImSpinner2 } from 'react-icons/im'
+
+import ProductsService from "../../services/ProductsService"
+import { MovieCard } from "../MovieCard"
+
+import * as S from './styles'
+
 export const MoviesList = () => {
+  const [products, setProducts] = useState([])
+  const [loading, setLoading] = useState(true)
+
+  useEffect(() => {
+    setLoading(true)
+    async function getProducts() {
+      const productsList =  await ProductsService.listProducts()
+      setLoading(false)
+      setProducts(productsList)
+    }
+
+    getProducts()
+  }, [])
+
   return (
-    <h1>Movies List</h1>
+    <S.MoviesList>
+      {loading ? (
+        <S.Loading>
+          <ImSpinner2 size="55px" />
+        </S.Loading>
+      ) : (
+        products.map(product => (
+          <MovieCard
+            title={product.title}
+            price={product.price}
+            image={product.image}
+            key={product.id}
+          />
+        ))
+      )}
+    </S.MoviesList>
   )
 }
--- wefit-test-react/wemovies/src/components/MoviesList/styles.js
+++ wefit-test-react/wemovies/src/components/MoviesList/styles.js
@@ -1,5 +1,27 @@
 import styled from "styled-components";
 
 export const MoviesList = styled.div`
+  max-width: 100%;
+  display: flex;
+  flex-wrap: wrap;
+  align-items: center;
+  justify-content: center;
+  gap: 1.2rem;
+`
 
+export const Loading = styled.div`
+  height: calc(100vh - 8.3rem);
+  display: flex;
+  align-items: center;
+  justify-content: center;
+
+  @keyframes spinner {
+    to {
+      transform: rotate(360deg);
+    }
+  }
+
+  svg {
+    animation: spinner .6s linear infinite;
+  }
 `
