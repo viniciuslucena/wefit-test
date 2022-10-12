cbd71e047343dcd9b05e4b791ac97e4e3543d931
Vinicius <jviniiciuslucena@gmail.com>
2022-10-12 00:44:22 -0300

    feat: api timeout simulation

--- wefit-test-react/wemovies/src/components/MoviesList/index.jsx
+++ wefit-test-react/wemovies/src/components/MoviesList/index.jsx
@@ -1,25 +1,28 @@
-import { useEffect, useState } from "react"
-import { ImSpinner2 } from 'react-icons/im'
+import { useEffect, useState } from "react";
+import { ImSpinner2 } from "react-icons/im";
 
-import ProductsService from "../../services/ProductsService"
-import { MovieCard } from "../MovieCard"
+import ProductsService from "../../services/ProductsService";
+import { MovieCard } from "../MovieCard";
 
-import * as S from './styles'
+import * as S from "./styles";
 
 export const MoviesList = () => {
-  const [products, setProducts] = useState([])
-  const [loading, setLoading] = useState(true)
+  const [products, setProducts] = useState([]);
+  const [loading, setLoading] = useState(true);
 
   useEffect(() => {
-    setLoading(true)
-    async function getProducts() {
-      const productsList =  await ProductsService.listProducts()
-      setLoading(false)
-      setProducts(productsList)
-    }
+    setLoading(true);
+    // Simular o tempo de execução na API
+    setTimeout(() => {
+      async function getProducts() {
+        const productsList = await ProductsService.listProducts();
+        setLoading(false);
+        setProducts(productsList);
+      }
 
-    getProducts()
-  }, [])
+      getProducts();
+    }, "1000");
+  }, []);
 
   return (
     <S.MoviesList>
@@ -28,7 +31,7 @@ export const MoviesList = () => {
           <ImSpinner2 size="55px" />
         </S.Loading>
       ) : (
-        products.map(product => (
+        products.map((product) => (
           <MovieCard
             title={product.title}
             price={product.price}
@@ -38,5 +41,5 @@ export const MoviesList = () => {
         ))
       )}
     </S.MoviesList>
-  )
-}
+  );
+};
